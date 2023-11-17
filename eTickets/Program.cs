6d0aaabb685377e054ba09f6a90b5088bfda30ce
Manir Saker <manir.saker@gmail.com>
2023-11-17 17:39:46 +0100

    updated models, controllers, and views with migrations into sql server

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,9 +1,34 @@
+using eTickets.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
+
+// Seed initializer code
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        // Call your seed method here
+        AppDbInitializer.Seed(app);
+    }
+    catch (Exception ex)
+    {
+        // Handle any exceptions or logging here
+        Console.WriteLine("An error occurred while seeding the database: " + ex.Message);
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,3 +50,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
